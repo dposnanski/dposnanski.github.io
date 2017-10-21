d8cbc70ac7b8f0d2a3794baffacbf9d0780d4115
dponi <dposnanski@ymail.com>
2017-10-21 21:21:27 +0200

    2 Added rest of files to repo

---
+++ assets/js/scripts.js
@@ -0,0 +1,190 @@
+// Google Maps
+/*var marker;
+
+function initMap() {
+  var map = new google.maps.Map(document.getElementById('map'), {
+    zoom: 16,
+    center: {
+      lat: 45.522152,
+      lng: 9.593093
+    }
+  });
+
+  marker = new google.maps.Marker({
+    map: map,
+    draggable: false,
+    title: "Somewhere Here",
+    animation: google.maps.Animation.DROP,
+    position: {
+      lat: 45.522154,
+      lng: 9.593090
+    }
+  });
+  marker.addListener('click', toggleBounce);
+}
+
+function toggleBounce() {
+  if (marker.getAnimation() !== null) {
+    marker.setAnimation(null);
+  } else {
+    marker.setAnimation(google.maps.Animation.BOUNCE);
+  }
+}*/
+
+$(document).ready(function () {
+
+  // Facebook
+  (function (d, s, id) {
+    var js, fjs = d.getElementsByTagName(s)[0];
+    if (d.getElementById(id)) return;
+    js = d.createElement(s);
+    js.id = id;
+    js.src = "//connect.facebook.net/en_US/sdk.js#xfbml=1&version=v2.8&appId=1818132841733925";
+    fjs.parentNode.insertBefore(js, fjs);
+  }(document, 'script', 'facebook-jssdk'));
+
+  // Smooth scrolling
+  var $root = $('html, body');
+  $('.navbar-nav a').click(function () {
+    var href = $.attr(this, 'href');
+    $root.animate({
+      scrollTop: $(href).offset().top
+    }, 500, function () {
+      window.location.hash = href;
+    });
+    return false;
+  });
+
+  //Tooltips
+  $(function () {
+    $("#course").tooltip();
+  });
+
+  $(function () {
+    $('[data-toggle="tooltip"]').tooltip();
+  });
+
+  //Button
+  $("#button").on("click", function () {
+    var validateData = function (id) {
+      var field = $("#" + id).val();
+      if (field === "") {
+        $("#" + id).css("border", "1px solid #0DD6C7");
+        $("#visible-" + id).html("This field needs to be completed").show();
+        return false;
+      } else {
+        console.log(id + ": " + field);
+        $("#visible-" + id).hide();
+        return true;
+      }
+    };
+
+    var name = validateData("name");
+    var email = validateData("email");
+    var message = validateData("message");
+    var phone = $(".phone").val();
+    if (!name || !email || !message) {
+      $("#phone").css("border", "1px solid #CCC");
+    } else {
+      $(".message").hide();
+      $(".email").hide();
+      $(".phone").hide();
+      $(".name").hide();
+      $("#char-count").hide();
+      $("#visible-message").html("Great! Thanks for sending me a message, I'll get back to you soon :-)").show();
+    }
+    return false;
+  });
+
+  //Character Count
+  $(".message").on("keyup", function () {
+    var charCount = $(".message").val().length;
+    $("#char-count").html("You have typed " + charCount + " characters.");
+    if (charCount > 50) {
+      $("#char-count").css("color", "red");
+    } else {
+      $("#char-count").css("color", "#000");
+    }
+    console.log(charCount);
+  });
+
+  //Services
+  var i = 0;
+  while (services[i]) {
+    $("#services-detail").append("\
+      <div class='col-md-6 services-columns'>\
+        <h2 class='services-title'>" + services[i].title + "</h2>\
+        <p class='services-description'>" + services[i].description + "</p>\
+        <a href='" + services[i].web + "'><button type='button' class='btn btn-primary'>" + services[i].button + "</button></a>\
+        <p>" + services[i].startPrice + "</p>\
+      </div>\
+    ");
+    i++;
+  }
+
+  /*FAQs
+  var i = 0;
+  while (questions[i]) {
+    $("#accordion").append("\
+          <div class='panel panel-default'>\
+              <div class='panel-heading'>\
+                <h4 class='panel-title'>\
+                  <a data-toggle='collapse' data-parent='#accordion' href='#collapseOne'>\
+                  <i class='fa fa-angle-right' aria-hidden='true'></i>" + questions[i].q + "\
+                  </a>\
+                </h4>\
+              </div>\
+              <div id='collapseOne' class='panel-collapse collapse'>\
+                <div class='panel-body'>" + questions[i].a + "</div>\
+              </div></div>\
+          ");
+    i++;
+  }*/
+
+
+
+  //Portfolio
+  var i = 0;
+  while (works[i]) {
+    $("#projects").append("\
+      <div class='col-sm-6 col-md-4 col-lg-4 projects'>\
+        <a href='" + works[i].web + "' class='work-img'>\
+          <img class='img-responsive' src='" + works[i].pic + "' alt='" + works[i].alt + "'>\
+          <span class='info'>\
+            <h3 class='proj-title'>" + works[i].title + "</h3>\
+            <p>" + works[i].description + "</p>\
+          <span>\
+        </a>\
+      </div>\
+    ");
+    i++;
+  }
+  $(".work-img").mouseenter(function () {
+    $(".info", this).show();
+  }).mouseleave(function () {
+    $(".info", this).hide();
+  });
+
+
+  //Parallax Effect
+  /*var $window = $(window);
+
+  $('section[data-type="background"]').each(function () {
+
+    var $bgobj = $(this);
+
+    $(window).scroll(function () {
+
+      var yPos = -($window.scrollTop() / $bgobj.data('speed'));
+
+      var coords = '50% ' + yPos + 'px';
+
+      $bgobj.css({
+        backgroundPosition: coords
+      });
+
+    });
+
+  });*/
+
+});
---
+++ assets/js/vars.js
@@ -0,0 +1,84 @@
+var works = [
+  {
+    title: "El Jardín",
+    description: "WordPress Website Design",
+    pic: "../assets/img/relaunch.png",
+    alt: "WordPress Website Design: El Jardín Vegano",
+    web: "jardin.html"
+  },
+  {
+    title: "Portraits",
+    description: "Photo Editing",
+    pic: "../assets/img/photoedit.jpg",
+    alt: "Photo Editing: Sales Personnel Portraits",
+    web: "photoedits"
+  },
+  {
+    title: "catCulator",
+    description: "JS + jQuery Exercise",
+    pic: "../assets/img/catculator.jpg",
+    alt: "JS/jQuery: The catCulator",
+    web: "catculator"
+  },
+  {
+    title: "Qeep",
+    description: "App Translation",
+    pic: "../assets/img/coming%20soon.jpg",
+    alt: "App Translation: Qeep",
+    web: "qeep"
+  },
+  {
+    title: "Avory",
+    description: "App Translation",
+    pic: "../assets/img/coming%20soon.jpg",
+    alt: "App Translation: Avory",
+    web: "avory"
+  },
+  {
+    title: "Next",
+    description: "Something Cool",
+    pic: "../assets/img/blueglassballs.jpg",
+    alt: "Next Project: Something Cool",
+    web: "cool"
+  }
+];
+
+var services = [
+  {
+    title: "Translation",
+    description: "I specialize in Spanish translation, specifically multimedia content such as websites, films, software, and video games. If you need subtitling, dubbing, localization, transcreation, or proofreading services, you've come to the right place!",
+    button: "More &raquo;",
+    web: "translation.html",
+    startPrice: "Prices start at 25 €"
+  },
+  {
+    title: "WP Development",
+    description: "WordPress is the most used CMS out there. It's a very customizable tool which can help you maintain your online presence (even by yourself). But don't worry, if you want to focus on other things, I can maintain it for you!",
+    button: "More &raquo;",
+    web: "webdev.html",
+    startPrice: "Prices start at 250 €"
+  }
+];
+
+/*var questions = [
+  {
+    q: "What inspired you to become a web developer?",
+    a: "I had made a couple of personal websites back when I was 16 and I really enjoyed it. I remember a lot of sleepless nights playing around with the code trying to make it look exactly how I wanted it to. I studied programming on my last high-school semester but then, I went on to college and studied a completely different thing. I ended up working at Costumer Service, Translation and Tourism related companies. I really liked translation, but I remembered those days when I was a teenager and how passionate I was about making my websites. I had made an online store with Prestashop and then moved it to Wordpress, and I remembered my teenage years, and building websites with passion. So I then decided it was the path I wanted to take!"
+  },
+  {
+    q: "What kind of projects do you like to work on?",
+    a: "I like to work on creative, beautiful projects that make the world a better place. Be it making people feel happy through humor, giving people the chance to store and share their memories, informing and educating them, aiding other people, helping animals, you name it!"
+  },
+  {
+    q: "What do you need in order to start working on a project?",
+    a: "For translation related services: I would need the document, movie, website or project you need translated and the preferred delivery date. For web development services: I would need to know what kind of website I'd be developing and which content you would like to publish. Is it a photography portfolio website? An online store? A landing page? A blog? If you have an idea of the sections on your website, along with the content and pictures, that would be great!"
+  },
+  {
+    q: "What is your preferred communication method and how often do you keep in touch while working on a project?",
+    a: "I find written communication forms to be better, I prefer email and WhatsApp, we can Skype to arrange the details of the project though. I like having everything documented so I can look back, review and double check projects :-) We would arrange milestone projects, and, depending on the project's length and complexity, I'd keep you up to date at least once a week."
+  },
+  {
+    q: "What is your average project turnaround?",
+    a: "It really depends on the complexity of the project, but, if you provide me with all the necessary information, pictures and content from the start, it will go a lot faster :-) My estimate is around one week."
+  }
+];*/
